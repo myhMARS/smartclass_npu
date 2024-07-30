9115643082a8c86d0374c022c51faa65a2ce895a
mengyonghao <1533512157@qq.com>
2024-07-30 20:41:59 +0800

    行为管理线程更新

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import configparser
+import datetime
 import threading
 import time
 from copy import deepcopy
@@ -8,7 +9,7 @@ import numpy as np
 from PIL import ImageGrab
 
 from apis import Api
-from model_utils import FaceTracker, plot_one_box
+from model_utils import FaceTracker, ActionManager, plot_one_box
 from run_model.face_model import FaceRecognition
 
 config = configparser.ConfigParser()
@@ -77,12 +78,17 @@ class VideoProcess(threading.Thread):
         self.video_size = video_size
         self.ready = False
         self.face_frame = np.zeros((video_size[0], video_size[1], 3), dtype=np.uint8)
-        self.frame = np.zeros((video_size[0], video_size[1], 3), dtype=np.uint8)
         self.first_frame = np.zeros((video_size[0], video_size[1], 3), dtype=np.uint8)
+
+        self.frame = np.zeros((video_size[0], video_size[1], 3), dtype=np.uint8)
+        self.timestamp = 0
         self.daemon = True
 
     def get_frame(self):
         return deepcopy(self.frame)
+
+    def get_timestamp(self):
+        return int(self.timestamp)
 
     def get_fps(self):
         cap = cv2.VideoCapture(self.camera_id)
@@ -107,9 +113,11 @@ class VideoProcess(threading.Thread):
         while not thread_exit:
             ret, frame = cap.read()
             if ret:
+                timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
                 frame = cv2.resize(frame, self.video_size)
                 thread_lock_video.acquire()
                 self.frame = frame
+                self.timestamp = timestamp
                 thread_lock_video.release()
             else:
                 thread_exit = True
@@ -119,56 +127,61 @@ class VideoProcess(threading.Thread):
 
 def main():
     global thread_exit
-
     camera_id = './test_video2.mp4'
     video_size = (1920, 1080)
     model = Api()
-    video_thread = VideoProcess(camera_id, video_size)
-    faceDetector = FaceRecognition()
-    faceTracker = FaceTracker()
+    video_thread = VideoProcess(camera_id, video_size)  # 视频流线程
+    faceDetector = FaceRecognition()  # 人脸识别
+    faceTracker = FaceTracker()  # 目标追踪
+    action_queue = []
+    manager_thread_lock = threading.Lock()
+    actionManager = ActionManager(action_queue, manager_thread_lock)  # 行为管理线程
+    actionManager.start()
     video_thread.start()
+
+    start_time = time.time()
     thread_lock_video.acquire()
     face_frame = video_thread.get_face_frame()
     frame = deepcopy(video_thread.first_frame)
     thread_lock_video.release()
+
     faces = faceDetector.get_faces(face_frame)
     for box in faces:
         box_location = format_box_location((box['xmin'], box['ymin'], box['xmax'], box['ymax']))
         faceTracker.create_tracker(frame, box['name'], box_location)
-        print(box['name'])
-    frame = faceTracker.update(frame)
-    # cv2.imshow('face', face_frame)
-    # cv2.imshow('face_frame', frame)
-    #
-    #     cv2.imshow('face', frame)
-
-    # out_win = 'face'
-    # cv2.namedWindow(out_win, cv2.WINDOW_NORMAL)
-    # cv2.setWindowProperty(out_win, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    # cv2.imshow('face', frame)
-    # cv2.waitKey(0)
+    faceTracker.update(frame)
 
     while not thread_exit:
         thread_lock_video.acquire()
         frame = video_thread.get_frame()
+        timestamp = video_thread.get_timestamp()
         thread_lock_video.release()
         action_boxes = model.process_frame(frame, video_size)
+        a = time.time()
         frame = faceTracker.update(frame)
         names = faceTracker.names
         face_boxes = faceTracker.boxes
         for action_box in action_boxes:
             index = find_max_overlap_index(action_box, face_boxes)
-            frame = plot_one_box([action_box['xmin'], action_box['ymin'], action_box['xmax'], action_box['ymax']],
+            action = {
+                'name': names[index] if index != -1 else 'Unknown',
+                'action': action_box['name'],
+                'location': [action_box['xmin'], action_box['ymin'], action_box['xmax'], action_box['ymax']],
+                'timestamp': timestamp,
+            }
+            action_queue.append(action)
+            frame = plot_one_box(action['location'],
                                  frame,
-                                 label=f'{action_box['name']} name:{names[index] if index != -1 else 'None'}',
-                                 color=COLORS[action_box['name']])
+                                 label=f'{action["action"]} name:{action["name"] if index != -1 else 'Unknown'}',
+                                 color=COLORS[action['action']])
+        # print('cost:', time.time() - a)
         out_win = "smartclass"
         cv2.namedWindow(out_win, cv2.WINDOW_NORMAL)
         cv2.setWindowProperty(out_win, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         cv2.imshow('smartclass', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             thread_exit = True
-
+    runtime = time.time() - start_time
     cv2.destroyAllWindows()
     video_thread.join()
 
